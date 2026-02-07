d2a655943e326e9c6ff89f640bc0a4930d15c99f
mubonassi <115490684+mubonassi@users.noreply.github.com>
2026-02-07 18:24:03 +0000

    asodapodskapsdkp

--- desafios/desafio3.py
+++ desafios/desafio3.py
@@ -1,2 +1,15 @@
 #Faça um algortimo
-#que converta de graus célsius para fahrenheit e kelvin
+#Que converta de graus célsius para fahrenheit e kelvin
+#- Equações -
+#Fahrenheit: (C * 9/5) + 32
+#Kelvin: C + 273.15
+
+celsius = float(input("Digite a temperatura em ºC: "))
+fahrenheit = (celsius * 9/5) + 32
+kelvin = celsius + 273.15
+
+print("-"*10)
+print("--Temperaturas--")
+print(f"Celsius: {celsius} ºC")
+print(f"Fahrenheint: {fahrenheit} ºF")
+print(f"Kelvin: {kelvin} K")
